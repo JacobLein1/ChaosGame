635713489f9c31626b20f661a2a419619fb32d1c
Madeleine Negård <madelne@stud.ntnu.no>
2024-02-22 16:03:09 +0100

    Create Matrix2x2

---
+++ src/main/java/Matrix2x2.java
@@ -0,0 +1,23 @@
+public class Matrix2x2 {
+    private double a00;
+    private double a01;
+    private double a10;
+    private double a11;
+
+    //Constructor
+    Matrix2x2(double a00, double a01, double a10, double a11){
+        this.a00 = a00;
+        this.a01 = a01;
+        this.a10 = a10;
+        this.a11 = a11;
+    }
+    //method for multiplying a matrix by a vector
+    //@param other - the vector to multiply by
+    //return - the product of the matrix and the vector (a vector)
+    public Vector2D multiply(Vector2D other){
+        double ax0 = a00*other.getX0() + a01*other.getX1();
+        double ax1 = a10*other.getX0() + a11*other.getX1();
+        return new Vector2D(ax0, ax1);
+    }
+
+}
