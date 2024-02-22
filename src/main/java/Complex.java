7d67506ad41259d76c962f49cc999965a7f2761c
Madeleine Negård <madelne@stud.ntnu.no>
2024-02-22 16:16:17 +0100

    Vmk

---
+++ src/main/java/Complex.java
@@ -0,0 +1,10 @@
+import java.lang.Math;
+public class Complex extends Vector2D{
+    public Complex(double realPart,double imaginaryPart){
+        super(realPart,imaginaryPart);
+    }
+    //public Complex sqrt(){
+
+    //}
+
+}
