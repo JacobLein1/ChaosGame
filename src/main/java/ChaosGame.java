c41723b1841bcc3e30c79f56b4b85e96ecd32109
Jacob Lein <jacoble@stud.ntnu.no>
2024-03-24 17:29:11 +0100

    Fix runSteps()

--- src/main/java/ChaosGame.java
+++ src/main/java/ChaosGame.java
@@ -16,9 +16,31 @@ public class ChaosGame {
         return canvas;
     }
     public void runSteps(int steps){
+        Random dice = new Random();
+        Vector2D p1 = new Vector2D(50, 0);
+        Vector2D p2 = new Vector2D(0, 99);
+        Vector2D p3 = new Vector2D(99, 99);
+        currentPoint = new Vector2D(0,99);
+        canvas.putPixel(p1);
+        canvas.putPixel(p2);
+        canvas.putPixel(p3);
+        canvas.putPixel(currentPoint);
+
+
         for(int i = 0; i < steps; i++){
-            Transform2D transform = description.getTransforms().get(random.nextInt(description.getTransforms().size()));
-            currentPoint = transform.Transform(currentPoint);
+            int result = dice.nextInt(1, 3);
+            AffineTransform2D transform = new AffineTransform2D(new Matrix2x2(0, currentPoint.getX0()-1/(canvas.getMinCoords().getX1()-canvas.getMaxCoords().getX1()), currentPoint.getX1()-1/(canvas.getMaxCoords().getX0()-canvas.getMinCoords().getX0()), 0), new Vector2D((
+                    ((currentPoint.getX0())-1)*canvas.getMaxCoords().getX1())/(canvas.getMaxCoords().getX1()-canvas.getMinCoords().getX1())
+                    ,((currentPoint.getX1()-1)*canvas.getMinCoords().getX0())/(canvas.getMinCoords().getX0()-canvas.getMaxCoords().getX0())));
+            if(result == 1){
+                currentPoint = transform.Transform(p1);
+            }
+            else if(result == 2){
+                currentPoint = transform.Transform(p2);
+            }
+            else if(result == 3){
+                currentPoint = transform.Transform(p3);
+            }
             canvas.putPixel(currentPoint);
         }
     }
