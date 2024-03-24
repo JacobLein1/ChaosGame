a8ac80788a01e39bec84418f03566135d1bec04c
Jacob Lein <jacoble@stud.ntnu.no>
2024-03-24 17:28:52 +0100

    Edit ChaosCanvas

--- src/main/java/ChaosCanvas.java
+++ src/main/java/ChaosCanvas.java
@@ -1,11 +1,11 @@
 import javafx.scene.transform.Affine;
 
 public class ChaosCanvas {
-    private int[][] canvasArray;
     private int width;
     private int height;
-    private Vector2D minCoords;
-    private Vector2D maxCoords;
+    private int[][] canvasArray;
+    private final Vector2D minCoords;
+    private final Vector2D maxCoords;
     private AffineTransform2D transformCoordsToIndices;
 
     public ChaosCanvas(int width, int height, Vector2D minCoords, Vector2D maxCoords){
@@ -13,17 +13,17 @@ public class ChaosCanvas {
         this.height = height;
         this.minCoords = minCoords;
         this.maxCoords = maxCoords;
-
+        this.canvasArray = new int[width][height];
     }
     public int getPixel(Vector2D point){
-        int x0 = Integer.parseInt(Double.toString(point.getX0()));
-        int x1= Integer.parseInt(Double.toString(point.getX1()));
+        int x0 = (int) point.getX0();
+        int x1= (int) point.getX1();
 
         return canvasArray[x0][x1];
     }
     public void putPixel(Vector2D point){
-        int x0 = Integer.parseInt(Double.toString(point.getX0()));
-        int x1= Integer.parseInt(Double.toString(point.getX1()));
+        int x0 = (int) point.getX0();
+        int x1= (int) point.getX1();
 
         canvasArray[x0][x1] = 1;
     }
@@ -37,5 +37,24 @@ public class ChaosCanvas {
                 canvasArray[i][j] = 0;
             }
         }
+    }
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < canvasArray.length; i++) {
+            for (int j = 0; j < canvasArray[i].length; j++) {
+                sb.append(canvasArray[i][j]).append(" "); // Legg til matriseelement og mellomrom
+            }
+            sb.append("\n"); // Legg til linjeskift etter hver rad
+        }
+        return sb.toString();
+    }
+
+    public Vector2D getMinCoords() {
+        return minCoords;
+    }
+
+    public Vector2D getMaxCoords() {
+        return maxCoords;
     }
 }
