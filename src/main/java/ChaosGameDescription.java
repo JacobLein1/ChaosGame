ca11624e1f9c14e1b3c48fed0516ea121bfbee55
Jacob Lein <jacoble@stud.ntnu.no>
2024-03-24 17:29:41 +0100

    Fix bugs

--- src/main/java/ChaosGameDescription.java
+++ src/main/java/ChaosGameDescription.java
@@ -3,7 +3,7 @@ import java.util.List;
 public class ChaosGameDescription {
     private final Vector2D minCoords;
     private final Vector2D maxCoords;
-    private final List<Transform2D> transforms;
+    private List<Transform2D> transforms;
 
     public ChaosGameDescription(Vector2D minCoords, Vector2D maxCoords, List<Transform2D> transforms){
         this.minCoords = minCoords;
