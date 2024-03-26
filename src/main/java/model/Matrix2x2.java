2d64fa404220707778ed969a66016918294d4932
Madeleine Negård <madelne@stud.ntnu.no>
2024-03-26 10:58:38 +0100

    Made classes public

--- src/main/java/model/Matrix2x2.java
+++ src/main/java/model/Matrix2x2.java
@@ -9,7 +9,7 @@ public class Matrix2x2 {
     private double a11;
 
     //Constructor
-    Matrix2x2(double a00, double a01, double a10, double a11){
+    public Matrix2x2(double a00, double a01, double a10, double a11){
         this.a00 = a00;
         this.a01 = a01;
         this.a10 = a10;
--- src/main/java/model/Vector2D.java
+++ src/main/java/model/Vector2D.java
@@ -5,7 +5,7 @@ public class  Vector2D {
     private double x1;
 
     //Constructor
-    Vector2D(double x0, double x1) {
+    public Vector2D(double x0, double x1) {
         this.x0 = x0;
         this.x1 = x1;
     }
