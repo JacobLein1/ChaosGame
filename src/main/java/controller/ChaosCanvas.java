cc864c7362578daa3731c8f97ad2b1416b35bb92
Jacob Lein <jacoble@stud.ntnu.no>
2024-04-12 13:56:07 +0200

    Translation in constructor

--- src/main/java/controller/ChaosCanvas.java
+++ src/main/java/controller/ChaosCanvas.java
@@ -1,15 +1,17 @@
 package controller;
 
 import model.AffineTransform2D;
+import model.Matrix2x2;
 import model.Vector2D;
+import java.lang.Math;
 
 public class ChaosCanvas {
-    private int width;
-    private int height;
-    private int[][] canvasArray;
+    private final int width;
+    private final int height;
+    private final int[][] canvasArray;
     private final Vector2D minCoords;
     private final Vector2D maxCoords;
-    private AffineTransform2D transformCoordsToIndices;
+    private final AffineTransform2D transformCoordsToIndices;
 
     public ChaosCanvas(int width, int height, Vector2D minCoords, Vector2D maxCoords){
         this.width = width;
@@ -17,17 +19,28 @@ public class ChaosCanvas {
         this.minCoords = minCoords;
         this.maxCoords = maxCoords;
         this.canvasArray = new int[width][height];
+
+        double Ax1 = (this.width-1)/(minCoords.getX1()-maxCoords.getX1());
+        double Ax2 = (this.height-1)/(maxCoords.getX0()-minCoords.getX0());
+        double b1 = ((this.width-1)*maxCoords.getX1()) / (maxCoords.getX1()-minCoords.getX1());
+        double b2 = ((this.height-1)*minCoords.getX0())/(minCoords.getX0()-maxCoords.getX0());
+        Matrix2x2 matrixA = new Matrix2x2(0, Ax1, Ax2, 0);
+        Vector2D b = new Vector2D(b1, b2);
+        this.transformCoordsToIndices = new AffineTransform2D(matrixA, b);
+
     }
     public int getPixel(Vector2D point){
+        point = transformCoordsToIndices.Transform(point);
         int x0 = (int) point.getX0();
         int x1= (int) point.getX1();
 
         return canvasArray[x0][x1];
     }
     public void putPixel(Vector2D point){
+        point = transformCoordsToIndices.Transform(point);
         int x0 = (int) point.getX0();
         int x1= (int) point.getX1();
-
+        System.out.println("Drew: x0: " + x0 + " x1: " + x1);
         canvasArray[x0][x1] = 1;
     }
     public int[][] getCanvasArray(){
