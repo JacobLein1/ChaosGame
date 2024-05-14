f15d8ed7c9639aa4ccb0921f423b648871e76e75
Jacob Lein <jacoble@stud.ntnu.no>
2024-05-14 21:22:58 +0200

    Add javadoc to class and constructor

--- src/main/java/controller/ChaosGameDescription.java
+++ src/main/java/controller/ChaosGameDescription.java
@@ -1,15 +1,27 @@
 package controller;
 
+import model.AffineTransform2D;
+import model.JuliaTransform;
 import model.Transform2D;
 import model.Vector2D;
 
 import java.util.List;
 
+/**
+ * The type Chaos game description.
+ */
 public class ChaosGameDescription {
     private final Vector2D minCoords;
     private final Vector2D maxCoords;
     private List<Transform2D> transforms;
 
+    /**
+     * Instantiates a new Chaos game description.
+     *
+     * @param minCoords  the min coords
+     * @param maxCoords  the max coords
+     * @param transforms the transforms
+     */
     public ChaosGameDescription(Vector2D minCoords, Vector2D maxCoords, List<Transform2D> transforms){
         this.minCoords = minCoords;
         this.maxCoords = maxCoords;
