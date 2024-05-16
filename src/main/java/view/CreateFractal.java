26cb0f647e985bab0e3591b516bbf8639badfcaf
Madeleine Negård <madelne@stud.ntnu.no>
2024-05-16 15:07:41 +0200

    Superclasses for front end pages

---
+++ src/main/java/view/CreateFractal.java
@@ -0,0 +1,76 @@
+package view;
+
+import controller.ChaosGameDescription;
+import javafx.geometry.Insets;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.VBox;
+import model.AffineTransform2D;
+import model.Matrix2x2;
+import model.Transform2D;
+import model.Vector2D;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class CreateFractal extends Fractal{
+    private final List<Transform2D> newTransformations = new ArrayList<>();
+
+    public void setNewTransformationMenu(){
+        setMenu();
+        Label matrixLabel = new Label("Matrix");
+        TextField matrix00 = new TextField();
+        TextField matrix01 = new TextField();
+        TextField matrix10 = new TextField();
+        TextField matrix11 = new TextField();
+        matrix00.setPromptText("00");
+        matrix01.setPromptText("01");
+        matrix10.setPromptText("10");
+        matrix11.setPromptText("11");
+        Label vectorLabel = new Label("Vector");
+        TextField vectorX1 = new TextField();
+        TextField vectorX0 = new TextField();
+        vectorX0.setPromptText("X0");
+        vectorX1.setPromptText("X1");
+
+        Button saveTransformation = new Button("Save transformation");
+        saveTransformation.setOnAction(actionEvent -> {
+            AffineTransform2D affineTransform2D = new AffineTransform2D(
+                    new Matrix2x2(
+                            Double.parseDouble(matrix00.getText()),
+                            Double.parseDouble(matrix01.getText()),
+                            Double.parseDouble(matrix10.getText()),
+                            Double.parseDouble(matrix11.getText())),
+                    new Vector2D(
+                            Double.parseDouble(vectorX0.getText()),
+                            Double.parseDouble(vectorX1.getText())));
+            newTransformations.add(affineTransform2D);
+            matrix00.clear();
+            matrix01.clear();
+            matrix10.clear();
+            matrix11.clear();
+            vectorX0.clear();
+            vectorX1.clear();
+        });
+
+        Button saveAsFile = new Button("Save as file");
+        //noe her
+
+        VBox newTransformationMenu = new VBox(
+                vectorLabel, vectorX0, vectorX1,
+                matrixLabel, matrix00, matrix01, matrix10, matrix11,
+                saveTransformation, saveAsFile);
+        newTransformationMenu.setSpacing(5);
+        newTransformationMenu.setPadding(new Insets(0, 0, 0, 10));
+        VBox.setMargin(vectorX1, new Insets(5,0,15, 0));
+        VBox.setMargin(matrix11, new Insets(5,0,15,0));
+        VBox.setMargin(saveTransformation, new Insets(5,0,15, 0));
+        getRoot().setLeft(newTransformationMenu);
+    }
+
+    public List<Transform2D> getNewTransformations() {
+        return newTransformations;
+    }
+}
---
+++ src/main/java/view/Fractal.java
@@ -0,0 +1,92 @@
+package view;
+
+import controller.ChaosGame;
+import controller.ChaosGameDescription;
+import javafx.application.Application;
+import javafx.geometry.Insets;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.Border;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.HBox;
+import javafx.stage.Stage;
+
+public class Fractal extends Application {
+    private Stage stage;
+    private TextField stepsBox;
+    private String pageTitle;
+    private final BorderPane root = new BorderPane();
+    private ChaosGame chaosGame;
+    private ChaosGameDescription chaosGameDescription;
+    private int width;
+    private int height;
+
+    public void setMenu(){
+        Label numberLabel = new Label("Number of steps:");
+
+        stepsBox = new TextField();
+
+        Button showFractal = new Button("Show");
+        showFractal.setOnAction(actionEvent -> {
+            try {
+                chaosGame = new ChaosGame(chaosGameDescription, width, height);
+            } catch (Exception e) {
+                throw new RuntimeException(e);
+            }
+            displayFractal();
+        });
+
+        Button home = new Button("Home");
+        home.setOnAction(actionEvent -> {
+            HomePage homePage = new HomePage();
+            homePage.start(stage);
+        });
+
+        HBox menu = new HBox(numberLabel, stepsBox, showFractal, home);
+        HBox.setMargin(numberLabel, new Insets(20, 20, 10, 10));
+        HBox.setMargin(stepsBox, new Insets(20, 10, 20, 10));
+        HBox.setMargin(showFractal, new Insets(20, 10, 10, 20));
+        HBox.setMargin(home, new Insets(20, 10, 10, 20));
+
+        root.setTop(menu);
+    }
+
+    public void displayFractal(){
+        InitializeChaosGame initializeChaosGame = new InitializeChaosGame(chaosGame, Integer.parseInt(stepsBox.getText()), width, height);
+        ImageView image = initializeChaosGame.createFractalDisplay();
+        root.setCenter(image);
+    }
+
+    public void setPageTitle(String pageTitle) {
+        this.pageTitle = pageTitle;
+    }
+
+    public BorderPane getRoot() {
+        return root;
+    }
+
+    public void setChaosGameDescription(ChaosGameDescription chaosGameDescription) {
+        this.chaosGameDescription = chaosGameDescription;
+    }
+
+    public void setWidth(int width) {
+        this.width = width;
+    }
+
+    public void setHeight(int height) {
+        this.height = height;
+    }
+
+    @Override
+    public void start(Stage stage){
+        this.stage = stage;
+        Scene scene = new Scene(root, 1000, 800);
+
+        stage.setTitle(pageTitle);
+        stage.setScene(scene);
+        stage.show();
+    }
+}
