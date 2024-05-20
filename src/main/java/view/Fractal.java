ca78145ef289c9b622a0f1381750cfc720dd86c3
Madeleine Negård <madelne@stud.ntnu.no>
2024-05-20 22:08:30 +0200

    Save image as file

--- src/main/java/view/Fractal.java
+++ src/main/java/view/Fractal.java
@@ -10,11 +10,20 @@ import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+import javafx.scene.image.ImageView;
+import javafx.scene.image.PixelReader;
+import javafx.scene.image.WritableImage;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
+import javafx.stage.FileChooser;
 import javafx.stage.Stage;
 import model.Vector2D;
+
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
 
 public class Fractal extends Application {
     private Stage stage;
@@ -31,6 +40,8 @@ public class Fractal extends Application {
     private TextField maxCoordX0;
     private TextField maxCoordX1;
     private FractalDisplayObserver fractalDisplayObserver;
+    private ImageView imageView;
+    private Label imageFileText;
 
     public void setMenu(){
         Label numberLabel = new Label("Number of steps:");
@@ -43,6 +54,8 @@ public class Fractal extends Application {
         Label maxCoordLabel = new Label("Maximum coordinates:");
         maxCoordX0 = new TextField();
         maxCoordX1 = new TextField();
+        Button saveFractalAsImage = new Button("Save image");
+        imageFileText = new Label();
 
         minCoordX0.setPromptText("X0");
         minCoordX1.setPromptText("X1");
@@ -50,19 +63,28 @@ public class Fractal extends Application {
         maxCoordX1.setPromptText("X1");
 
         numberLabel.getStyleClass().add("menu-label");
-        steps.getStyleClass().add("steps-button");
+        steps.getStyleClass().add("steps-field");
         showFractal.getStyleClass().add("big-menu-button");
         home.getStyleClass().add("big-menu-button");
         minCoordLabel.getStyleClass().add("menu-label");
-        minCoordX0.getStyleClass().add("menu-button");
-        minCoordX1.getStyleClass().add("menu-button");
+        minCoordX0.getStyleClass().add("menu-field");
+        minCoordX1.getStyleClass().add("menu-field");
         maxCoordLabel.getStyleClass().add("menu-label");
-        maxCoordX0.getStyleClass().add("menu-button");
-        maxCoordX1.getStyleClass().add("menu-button");
+        maxCoordX0.getStyleClass().add("menu-field");
+        maxCoordX1.getStyleClass().add("menu-field");
+        saveFractalAsImage.getStyleClass().add("big-menu-button");
 
         home.setOnAction(actionEvent -> {
             HomePage homePage = new HomePage();
             homePage.start(stage);
+        });
+
+        saveFractalAsImage.setOnAction(actionEvent -> {
+            try {
+                saveAsImage();
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
         });
 
         VBox minVector = new VBox(minCoordX0, minCoordX1);
@@ -75,17 +97,21 @@ public class Fractal extends Application {
         HBox.setMargin(steps, new Insets(10,0,20,34));
 
         VBox inputBoxes = new VBox(stepsBox, vectorBox);
-        HBox buttonBox = new HBox(showFractal, home);
-        buttonBox.setAlignment(Pos.BOTTOM_RIGHT);
+        HBox buttonBox = new HBox(showFractal, saveFractalAsImage, home);
+        VBox buttonAndTextBox = new VBox(buttonBox, imageFileText);
+        buttonAndTextBox.setAlignment(Pos.BOTTOM_RIGHT);
+        imageFileText.setAlignment(Pos.BOTTOM_RIGHT);
 
-        HBox menu = new HBox(inputBoxes, buttonBox);
-        HBox.setMargin(buttonBox, new Insets(0,0,0,250));
-        HBox.setMargin(numberLabel, new Insets(20, 20, 10, 10));
-        HBox.setMargin(stepsBox, new Insets(20, 10, 20, 10));
-        HBox.setMargin(minCoordLabel, new Insets(20, 20, 10, 10));
-        HBox.setMargin(maxCoordLabel, new Insets(20, 20, 10, 10));
-        HBox.setMargin(showFractal, new Insets(20, 10, 10, 20));
-        HBox.setMargin(home, new Insets(20, 10, 10, 20));
+        HBox menu = new HBox(inputBoxes, buttonAndTextBox);
+        HBox.setMargin(buttonAndTextBox, new Insets(0,0,0,100));
+        HBox.setMargin(numberLabel, new Insets(20,20,10,10));
+        HBox.setMargin(stepsBox, new Insets(20,10,20,10));
+        HBox.setMargin(minCoordLabel, new Insets(20,20,10,10));
+        HBox.setMargin(maxCoordLabel, new Insets(20,20,10,10));
+        HBox.setMargin(showFractal, new Insets(20,10,10,20));
+        HBox.setMargin(home, new Insets(20,10,10,20));
+        HBox.setMargin(saveFractalAsImage, new Insets(20,10,10,20));
+        VBox.setMargin(imageFileText, new Insets(0,20,0,0));
         menu.getStyleClass().add("menu-background");
 
         root.setTop(menu);
@@ -108,7 +134,47 @@ public class Fractal extends Application {
     public void displayFractal(){
         fractalDisplayObserver = new FractalDisplayObserver(chaosGame, Integer.parseInt(steps.getText()), width, height);
         fractalDisplayObserver.updateGame();
-        root.setCenter(fractalDisplayObserver.getFractalImageView());
+        imageView = fractalDisplayObserver.getFractalImageView();
+        root.setCenter(imageView);
+    }
+
+    public void saveAsImage() throws IOException {
+        WritableImage writableImage = new WritableImage(height, width);
+        imageView.snapshot(null, writableImage);
+
+        PixelReader pixelReader = writableImage.getPixelReader();
+        BufferedImage bufferedImage = new BufferedImage(height, width, BufferedImage.TYPE_INT_ARGB);
+
+        for (int y = 0; y < width; y++) {
+            for (int x = 0; x < height; x++) {
+                bufferedImage.setRGB(x, y, pixelReader.getArgb(x, y));
+            }
+        }
+
+        try {
+            if (getChaosGameDescription() == null) {
+                throw new IllegalArgumentException("No fractal to save");
+            }else {
+                //User selects file path
+                FileChooser fileChooser = new FileChooser();
+                fileChooser.setTitle("Save image File");
+                FileChooser.ExtensionFilter extFilter = new FileChooser.ExtensionFilter("PNG files (*.png)", "*.png");
+                fileChooser.getExtensionFilters().add(extFilter);
+                File file = fileChooser.showSaveDialog(null);
+
+                if (file != null) {
+                    imageFileText.setText(file.getAbsolutePath());
+                    imageFileText.setText("File saved to " + imageFileText.getText());
+
+                    ImageIO.write(bufferedImage, "png", file);
+                }
+                else {
+                    imageFileText.setText("No path selected");
+                }
+            }
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
     }
 
     public void setPageTitle(String pageTitle) {
@@ -157,6 +223,27 @@ public class Fractal extends Application {
 
     public Vector2D getMaxCoords(){
         return new Vector2D(Double.parseDouble(maxCoordX0.getText()), Double.parseDouble(maxCoordX1.getText()));
+    }
+
+    public void setAffineCoords(){
+        minCoordX0.setText("0");
+        minCoordX1.setText("0");
+        maxCoordX0.setText("1");
+        maxCoordX1.setText("1");
+    }
+
+    public void setBarnsleyCoords(){
+        minCoordX0.setText("-2.65");
+        minCoordX1.setText("0");
+        maxCoordX0.setText("2.65");
+        maxCoordX1.setText("10");
+    }
+
+    public void setJuliaCoord(){
+        minCoordX0.setText("-1.6");
+        minCoordX1.setText("-1");
+        maxCoordX0.setText("1.6");
+        maxCoordX1.setText("1.1");
     }
 
     @Override
