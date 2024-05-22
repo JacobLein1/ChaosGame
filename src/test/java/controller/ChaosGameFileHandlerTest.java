1fd5caf2ef13905921c161ac59aec7fffe784ef1
Jacob Lein <jacoble@stud.ntnu.no>
2024-05-22 08:39:08 +0200

    Move ChaosGameDescription to controller folder

--- src/test/java/controller/ChaosGameFileHandlerTest.java
+++ src/test/java/controller/ChaosGameFileHandlerTest.java
@@ -3,7 +3,6 @@ package controller;
 import model.*;
 import org.junit.jupiter.api.*;
 
-import java.io.BufferedWriter;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
